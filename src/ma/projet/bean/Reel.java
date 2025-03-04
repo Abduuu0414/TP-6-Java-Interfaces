78386a79d2d4831d8595535aaff9cc418990f4a9
Abderrahman BOUANANI <bouananiabderahmen@gmail.com>
2025-03-04 13:57:52 +0000

    surcharge des methodes (variadic)

--- src/ma/projet/bean/Reel.java
+++ src/ma/projet/bean/Reel.java
@@ -4,45 +4,55 @@ import ma.projet.inter.IAffichage;
 import ma.projet.inter.IOperation;
 import ma.projet.abst.Affichage;
 
-public class Reel extends Affichage implements IAffichage, IOperation<Reel>{
-	
-	protected double x;
-	
-	public Reel(double x) {
-		this.x = x;
-	}
+public class Reel extends Affichage implements IAffichage, IOperation<Reel> {
 
+    protected double x;
 
-	public double getReel() {
-		return x;
-	}
+    public Reel(double x) {
+        this.x = x;
+    }
 
+    public double getReel() {
+        return x;
+    }
 
-	public void setReel(double x) {
-		this.x = x;
-	}
+    public void setReel(double x) {
+        this.x = x;
+    }
 
+    @Override
+    public Reel plus(Reel o) {
+        return new Reel(this.x + o.getReel());
+    }
 
-	@Override
-	public Reel plus(Reel o) {
-		return new Reel(this.x + o.getReel());
-	}
+    @Override
+    public Reel moins(Reel o) {
+        return new Reel(this.x - o.getReel());
+    }
 
-	@Override
-	public Reel moins(Reel o) {
-		return new Reel(this.x + o.getReel());
-	}
+    public double plus(double... x) {
+        double somme = this.x;
+        for (double valeur : x) {
+            somme += valeur;
+        }
+        return somme;
+    }
 
-	@Override
-	public String affiche() {
-		return String.format("%.0f", x);
-	}
+    public double moins(double... x) {
+        double resultat = this.x;
+        for (double valeur : x) {
+            resultat -= valeur;
+        }
+        return resultat;
+    }
 
+    @Override
+    public String affiche() {
+        return String.format("%.2f", x);
+    }
 
-	@Override
-	public String affiche2() {
-		return String.format("%.0f", x);
-	}
-
-	
+    @Override
+    public String affiche2() {
+        return String.format("%.2f", x);
+    }
 }
--- src/ma/projet/test/TestComplex.java
+++ src/ma/projet/test/TestComplex.java
@@ -17,7 +17,13 @@ public class TestComplex {
 		Reel r3 = r1.plus(r2);
 		Reel r4 = r1.moins(r2);
 		
+        double r1PlusVariadic = r1.plus(3, 5, 2.5); // 4 + 3 + 5 + 2.5 = 14.5
+        double r1MoinsVariadic = r1.moins(1, 2, 0.5); // 4 - 1 - 2 - 0.5 = 0.5
+		
 		System.out.println(" c1 : " + c1.affiche() + " ,c2 : " + c2.affiche() + " ,c3 : " + c3.affiche() + " ,c4 : " + c4.affiche());
 		System.out.println(" r1 : " + r1.affiche() + " ,r2 : " + r2.affiche() + " ,r3 : " + r3.affiche() + " ,r4 : " + r4.affiche());
+		
+		System.out.println("r1.plus(3, 5, 2.5) = " + r1PlusVariadic);
+        System.out.println("r1.moins(1, 2, 0.5) = " + r1MoinsVariadic);
 	}
 }
